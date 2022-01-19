815e4b31c32c13ceffa33dac2b0e3bbbbc363ebf
Luiz Chirutti <luizchirutti@gmail.com>
2022-01-19 15:00:05 -0300

    create controller Event

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -3,27 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
     //
     public function index(){
 
-        $nome = "Luiz";
-        $idade = 25;
+        $events = Event::all();
 
-        $arr = [10,20,30,40,50];
-
-        $nomes = ["Luiz", "Lucas", "Lorena", "Luca"];
-
-        return view('welcome',
-        [
-            'nome' => $nome,
-            'idade2' => $idade,
-            'profissao' => "Programador",
-            'arr' => $arr,
-            'nomes' => $nomes
-        ]);
+        return view('welcome',['events' => $events]);
 
     }
 
